09ce1e91332fab3c4d07ead16d1319de4d852ac1
Irad Doron <irad.doron@gmail.com>
2022-10-13 15:12:09 +0300

    modify data structure

--- data/chordProgressions.mjs
+++ data/chordProgressions.mjs
@@ -3,45 +3,105 @@ const chordProgressions = [
 		groupTitle: 'Diatonic Major',
 		collections: [
 			{
+				id: 1,
 				beats: 4,
 				collectionStructure: ['T', 'T', 'SD', 'D'],
 				structureString: 'T-T-SD-D',
 				progressions: [
-					{ rootPosition: 'I-I-IV-V', inversions: ['I-I6-IV-V'] },
-					{ rootPosition: 'I-vi-IV-V', inversions: ['I-iv6-IV-V'] },
-					{ rootPosition: 'I-vi-ii-V', inversions: ['I-vi6-ii-V'] },
-					{ rootPosition: 'I-iii-IV-V', inversions: ['I-iii6-IV-V'] },
-					{ rootPosition: 'I-vi-ii-V', inversions: ['I-vi6-ii-V'] },
+					{
+						id: 1,
+						rootPosition: ['I', 'I', 'IV', 'V'],
+						inversions: [['I', 'I6', 'IV', 'V']],
+					},
+					{
+						id: 2,
+						rootPosition: ['I', 'vi', 'IV', 'V'],
+						inversions: [['I', 'iv6', 'IV', 'V']],
+					},
+					{
+						id: 3,
+						rootPosition: ['I', 'vi', 'ii', 'V'],
+						inversions: [['I', 'vi6', 'ii', 'V']],
+					},
+					{
+						id: 4,
+						rootPosition: ['I', 'iii', 'IV', 'V'],
+						inversions: [['I', 'iii6', 'IV', 'V']],
+					},
+					{
+						id: 5,
+						rootPosition: ['I', 'vi', 'ii', 'V'],
+						inversions: [['I', 'vi6', 'ii', 'V']],
+					},
 				],
 			},
 			{
+				id: 2,
 				beats: 4,
 				collectionStructure: ['T', 'SD', 'D', 'T'],
 				structureString: 'T-SD-D-T',
 				progressions: [
-					{ rootPosition: 'I-ii-V-I', inversions: ['I-ii6-V-I'] },
-					{ rootPosition: 'I-ii-V-vi', inversions: ['I-ii6-V-vi'] },
-					{ rootPosition: 'I-ii-V-iii', inversions: ['I-ii6-V-iii'] },
-					{ rootPosition: 'I-IV-V-I', inversions: ['I-IV6-V-I'] },
-					{ rootPosition: 'I-IV-V-vi', inversions: ['I-IV6-V-vi'] },
-					{ rootPosition: 'I-IV-V-iii', inversions: ['I-IV6-V-iii'] },
+					{
+						id: 1,
+						rootPosition: ['I', 'ii', 'V', 'I'],
+						inversions: [['I', 'ii6', 'V', 'I']],
+					},
+					{
+						id: 2,
+						rootPosition: ['I', 'ii', 'V', 'vi'],
+						inversions: [['I', 'ii6', 'V', 'vi']],
+					},
+					{
+						id: 3,
+						rootPosition: ['I', 'ii', 'V', 'iii'],
+						inversions: [['I', 'ii6', 'V', 'iii']],
+					},
+					{
+						id: 4,
+						rootPosition: ['I', 'IV', 'V', 'I'],
+						inversions: [['I', 'IV6', 'V', 'I']],
+					},
+					{
+						id: 5,
+						rootPosition: ['I', 'IV', 'V', 'vi'],
+						inversions: [['I', 'IV6', 'V', 'vi']],
+					},
+					{
+						id: 6,
+						rootPosition: ['I', 'IV', 'V', 'iii'],
+						inversions: [['I', 'IV6', 'V', 'iii']],
+					},
 				],
 			},
 			{
+				id: 3,
 				beats: 4,
 				collectionStructure: ['T', 'SD', 'SD', 'D'],
 				structureString: 'T-SD-SD-D',
 				progressions: [
-					{ rootPosition: 'I-IV-ii-V', inversions: ['I-IV6-ii-V'] },
+					{
+						id: 1,
+						rootPosition: ['I', 'IV', 'ii', 'V'],
+						inversions: [['I', 'IV6', 'ii', 'V']],
+					},
 				],
 			},
 			{
+				id: 4,
 				beats: 4,
 				collectionStructure: ['T', 'SD', 'T', 'D'],
 				structureString: 'T-SD-T-D',
 				progressions: [
-					{ rootPosition: 'I-IV-vi-V', inversions: ['I-IV6-vi-V'] },
-					{ rootPosition: 'I-IV-iii-V', inversions: ['I-IV6-iii-V'] },
+					{
+						id: 1,
+						rootPosition: ['I', 'IV', 'vi', 'V'],
+						inversions: [['I', 'IV6', 'vi', 'V']],
+					},
+					{
+						id: 2,
+						rootPosition: ['I', 'IV', 'iii', 'V'],
+						inversions: [['I', 'IV6', 'iii', 'V']],
+					},
 					,
 				],
 			},
